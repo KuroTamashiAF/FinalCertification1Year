08d07c2a10d87125675df02de27e99375d37899a
Fedorov <chugun777akk@mail.ru>
2023-03-06 08:40:48 +0700

    Add cls Cat, Dog in HumanFriends.sql

--- SQLSheet/HumanFriends.sql
+++ SQLSheet/HumanFriends.sql
@@ -5,8 +5,21 @@ use HumanFriends;
 ## Создаем таблицы из схемы 
 CREATE TABLE Cat
 (
-
-
-
-
+Cat_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+Cat_name VARCHAR(10) NOT NULL, 
+Cat_age INT, 
+Cat_Date_Bith DATE, 
+Cat_sound VARCHAR(5) NOT NULL, 
+CAt_methods VARCHAR(50)
 );
+## DROP TABLE Cat;
+CREATE TABLE Dog
+(
+Dog_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+Dog_name VARCHAR(10) NOT NULL, 
+Dog_age INT, 
+Dog_Date_Bith DATE, 
+Dog_sound VARCHAR(5) NOT NULL, 
+Dog_methods VARCHAR(50)
+);
+## DROP TABLE Cat;
