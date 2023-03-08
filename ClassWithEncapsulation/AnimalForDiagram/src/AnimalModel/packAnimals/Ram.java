455ac165a1a97b70d018c9428e6d143382c5ea85
Fedorov <chugun777akk@mail.ru>
2023-03-08 15:20:27 +0700

    Add cls Ram

---
+++ ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Ram.java
@@ -0,0 +1,22 @@
+package AnimalModel.packAnimals;
+
+import AnimalModel.abctractAnimal;
+
+public class Ram extends abctractAnimal {
+    private int quantityWoolKg;
+
+    public Ram(String name, int age, String dateOfBith, int quantityWoolKg) {
+        super(name, age, dateOfBith);
+        this.quantityWoolKg = quantityWoolKg;
+    }
+
+    public int getQuantityWoolKg() {
+        return quantityWoolKg;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+
+                String.format(" Количество шерсти %d кг ", getQuantityWoolKg());
+    }
+}
