68b9b02bb6e05ab52595ba44643c3bcfd772c640
Fedorov <chugun777akk@mail.ru>
2023-03-08 16:06:07 +0700

    add class Camel

---
+++ ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Camel.java
@@ -0,0 +1,23 @@
+package AnimalModel.packAnimals;
+
+import AnimalModel.abctractAnimal;
+
+public class Camel extends abctractAnimal {
+
+    private int loadCapacity;
+
+    public Camel(String name, int age, String dateOfBith, int loadCapacity) {
+        super(name, age, dateOfBith);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                String.format(" Грузоподъемность %d кг ", getLoadCapacity());
+    }
+}
