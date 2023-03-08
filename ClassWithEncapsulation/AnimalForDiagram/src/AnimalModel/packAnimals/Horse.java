6708303d7adcedeb4732354b7bee90f04d5473dc
Fedorov <chugun777akk@mail.ru>
2023-03-08 15:23:06 +0700

    add cls Horse

--- ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Horse.java
+++ ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Horse.java
@@ -1,4 +1,22 @@
 package AnimalModel.packAnimals;
 
-public class Horse {
+import AnimalModel.abctractAnimal;
+
+public class Horse extends abctractAnimal {
+    private int loadCapacity;
+
+    public Horse(String name, int age, String dateOfBith, int loadCapacity) {
+        super(name, age, dateOfBith);
+        this.loadCapacity = loadCapacity;
+    }
+
+    public int getLoadCapacity() {
+        return loadCapacity;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+
+                String.format(" Грузоподъемность %d кг ", getLoadCapacity() );
+    }
 }
