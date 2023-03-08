3d3bac3ccce8bcead6de73c9a345fa93b87bcd91
Fedorov <chugun777akk@mail.ru>
2023-03-08 15:14:43 +0700

    Add cls Cow

--- ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Cow.java
+++ ClassWithEncapsulation/AnimalForDiagram/src/AnimalModel/packAnimals/Cow.java
@@ -1,4 +1,23 @@
 package AnimalModel.packAnimals;
 
-public class Cow {
+import AnimalModel.abctractAnimal;
+
+public class Cow extends abctractAnimal {
+
+    private boolean givesMilk;
+
+    public Cow(String name, int age, String dateOfBith, boolean givesMilk) {
+        super(name, age, dateOfBith);
+        this.givesMilk = givesMilk;
+    }
+
+    public boolean isGivesMilk() {
+        return givesMilk;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+
+                String.format("Дает молоко %s", isGivesMilk());
+    }
 }
